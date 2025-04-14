53ab526cfc28cd0322b568a673d8296b1e9d8114
AC-Gul <alexgulewich@gmail.com>
2025-04-14 00:57:08 -0700

    Sliding and hopping added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Numerics;
 using UnityEngine;
@@ -7,17 +8,28 @@ using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("States")]
     [SerializeField] private MovementParams walking;
     [SerializeField] private MovementParams running;
     [SerializeField] private MovementParams crouching;
     [SerializeField] MovementParams currentMovement;
     
+    [Header("Adjustable Parameters")]
     [SerializeField] float jumpForce = 5f;
+    [SerializeField] private float slideVelocity = 5f;
+    [SerializeField] private float slideUpBias = 2.0f;
+    [SerializeField] private float movementPauseTime = 1f;
 
+    // components and tracker variables
     private Rigidbody rb;
     Collider collider;
     [SerializeField] private Vector3 currentInput;
+    
+    // State tracking
     private bool moving = false;
+    private bool noMove = false;
+    private bool sliding = false;
+    private Vector3 cachedWallNormal = Vector3.zero;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -58,16 +70,29 @@ public class PlayerController : MonoBehaviour
         currentMovement = walking;
         
     }
+
+
+    bool IsGrounded()
+    {
+        Vector3 size = collider.bounds.extents;
+        RaycastHit hit;
+        return Physics.Raycast(transform.position, Vector3.down, out hit, size.y + 0.1f);
+    }
     
     
     void OnJump(InputValue value)
     {
-        RaycastHit hit;
-        Vector3 size = collider.bounds.extents;
-        if (!Physics.Raycast(transform.position, Vector3.down, out hit, size.y + 0.1f))
+        if (IsGrounded())
+        {
+            rb.AddForce(transform.up * jumpForce);
             return;
+        }
         
-        rb.AddForce(transform.up * jumpForce);
+        if (sliding)
+        {
+            WallHop();
+            return;
+        }
     }
     
     void OnCrouch(InputValue value)
@@ -80,6 +105,12 @@ public class PlayerController : MonoBehaviour
     {
         while (moving)
         {
+            if (noMove)
+            {
+                yield return new WaitForFixedUpdate();
+                continue;
+            }
+            
             rb.AddForce(currentInput * (currentMovement.acceleration * Time.deltaTime), ForceMode.Acceleration);
             
             // perserves the vertical speed
@@ -89,5 +120,66 @@ public class PlayerController : MonoBehaviour
             
             yield return new WaitForFixedUpdate();
         }
+    }
+    
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (IsGrounded())
+        {
+            sliding = false;
+            return;
+        }
+        
+        cachedWallNormal = other.GetContact(0).normal;
+        if (moving)
+        {
+            StartCoroutine(WallSlide());
+        }
+        
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        sliding = false;
+    }
+
+
+    void WallHop()
+    {
+        // hop off a wall
+        if (cachedWallNormal.x != 0 || cachedWallNormal.z != 0)
+        {
+            rb.AddForce(jumpForce * cachedWallNormal + (Vector3.up * slideVelocity), ForceMode.Acceleration);
+        }
+    }
+    
+    
+    // allow the player to slide down a wall
+    IEnumerator WallSlide()
+    {
+        // if the player is not on a wall, don't slide
+        if (!(cachedWallNormal.x != 0 || cachedWallNormal.z != 0 || IsGrounded()))
+            yield break;
+
+        // pause the movement for a bit to let hop occur
+        StartCoroutine(PauseMovement());
+        
+        sliding = true;
+        while (sliding)
+        {
+            // set slide to set speed
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, -slideVelocity, rb.linearVelocity.z);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+
+    // pause the movement for a bit
+    IEnumerator PauseMovement()
+    {
+        noMove = true;
+        yield return new WaitForSeconds(movementPauseTime);
+        noMove = false;
     }
 }
