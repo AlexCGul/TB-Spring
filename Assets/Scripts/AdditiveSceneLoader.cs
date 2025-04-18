25876d9cd2fb8a54003d8124df75a7960fd5e428
AC-Gul <alexgulewich@gmail.com>
2025-04-17 20:36:00 -0700

    Level 1 initial scene setup

---
+++ Assets/Scripts/AdditiveSceneLoader.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdditiveSceneLoader : MonoBehaviour
+{
+    [SerializeField] private string[] sceneNames;
+    private void OnValidate()
+    {
+        int newSceneCount = 0;
+
+        for (int x = 0; x < sceneNames.Length; x++)
+        {
+            string[] guids = AssetDatabase.FindAssets($"{sceneNames[x]} t:Scene");
+
+            if (guids.Length == 0)
+            {
+                Debug.LogError($"Scene '{sceneNames[x]}' not found.");
+                return;
+            }
+
+            string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+
+            if (EditorSceneManager.GetSceneByPath(scenePath).isLoaded)
+            {
+                Debug.Log($"Scene '{sceneNames[x]}' is already loaded.");
+                return;
+            }
+
+            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
+            Debug.Log($"Scene '{sceneNames[x]}' loaded additively.");
+        }
+        
+        Debug.Log(newSceneCount + " new scenes loaded");
+    }
+
+
+    private void Start()
+    {
+        for (int x = 0; x < sceneNames.Length; x++)
+        {
+            if (SceneExists(sceneNames[x]))
+            {
+                continue;
+            }
+            SceneManager.LoadScene(sceneNames[x], LoadSceneMode.Additive);
+        }
+    }
+
+
+    // Check if scene exists
+    private bool SceneExists(string sceneName)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.name == sceneName)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
